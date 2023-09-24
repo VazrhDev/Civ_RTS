c0429913f8ffd750ac669a798f6af66c360f9dc4
Enigma990 <jatin.pandey87@gmail.com>
2023-09-25 00:21:41 +0530

    First Commit

---
+++ Assets/_DAV/DAV.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DAV : MonoBehaviour
+{
+    public void OpenSingleplayer()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
